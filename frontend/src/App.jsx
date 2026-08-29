d95283d9b74cb6966aca8b822e6520daf305ae5d
prateek269 <kanhajshi203@gmail.com@gmail.com>
2026-08-29 16:43:55 +0000

    Fix frontend backend API URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-const API_URL = "http://127.0.0.1:5000";
+const API_URL = "http://localhost:5001";
 
 function App() {
   const [tasks, setTasks] = useState([]);
